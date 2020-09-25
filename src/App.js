9d37d7571dc5b0b1b6ba52751cd4a3bab9fd6936
jdsheridan02 <jdsheridan02@gmail.com>
2020-09-25 16:50:43 -0400

    added input field and function for filter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
@@ -7,48 +7,67 @@ import TableRow from "./components/TableRow";
 
 function App() {
   const [employee, setEmployee] = useState([]);
+  const [inputValue, updateInput] = useState("");
 
-  useEffect(() => { 
+  useEffect(() => {
     axios.get("http://dummy.restapiexample.com/api/v1/employees")
-    .then(data=>{
-      console.log(data.data.data);
-      setEmployee(data.data.data);
-    })
+      .then(data => {
+        console.log(data.data.data);
+        setEmployee(data.data.data);
+      })
 
   }, []);
 
   const onClick = () => {
-    let e=[...employee]
-    e.sort( (a,b) => b.id - a.id)
+    let e = [...employee]
+    e.sort((a, b) => b.id - a.id)
     setEmployee(e)
   }
 
+  const FilterInput = () => {
+
+    let filteredUsers = [...employees]
+    inputValue === ""
+      ? filteredUsers
+      : filteredUsers.filter(
+        ({ name: { first } }) =>
+          first.toLowerCase().indexOf(inputValue.toLowerCase()) >= 0
+      );
+
+    setEmployee(filteredUsers);
+  }
+
+
   return (
     <div className>
+      <form> 
+        <input type="text" name="name"></input>
+        <input type="submit" value="Submit" onClick={FilterInput}></input>
+      </form>
       <table class="table">
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">ID <button onClick={onClick}>sort</button></th>
-      <th scope="col">Name</th>
-      <th scope="col">Salary</th>
-      <th scope="col">Age</th>
-    </tr>
-  </thead>
-  <tbody>
-  {employee.map(e => {
-        return <TableRow 
-        id={e.id}
-        name={e.employee_name}
-        salary={e.employee_salary}
-        age={e.employee_age}
-        /> 
-        // use components to pass the information, the components will be how the HTML is designed, we are just using this area to pass information. 
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">ID <button onClick={onClick}>sort</button></th>
+            <th scope="col">Name</th>
+            <th scope="col">Salary</th>
+            <th scope="col">Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employee.map(e => {
+            return <TableRow
+              id={e.id}
+              name={e.employee_name}
+              salary={e.employee_salary}
+              age={e.employee_age}
+            />
+            // use components to pass the information, the components will be how the HTML is designed, we are just using this area to pass information. 
 
-      })}
-  </tbody>
-</table>
+          })}
+        </tbody>
+      </table>
 
-      
+
     </div>
   );
 }
